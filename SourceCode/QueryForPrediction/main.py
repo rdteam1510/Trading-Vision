771edc59e09c037bca627e4d2b933160018ea0f6
minhquach8 <minhquach8@gmail.com>
2021-12-17 14:44:59 +0700

    17/11/21 Minh updates QueryForPrediction

--- SourceCode/QueryForPrediction/main.py
+++ SourceCode/QueryForPrediction/main.py
@@ -12,19 +12,6 @@ headers = ["Time", "Name", "Closed"]
 main_df = pd.DataFrame(columns=headers)
 
 
-def read_stocks_text_file(namefile):
-    """
-    Read stocks from text file, remove end-line breaks, convert them into a list
-    :param: namefile
-    :return: a list of stocks
-    """
-    file = open(f"..\stockstickers\{namefile}.txt", "r")
-    content = file.read()
-    stocks_list = content.split(", ")
-    file.close()
-    return stocks_list
-
-
 def Processing(se):
     """
     Get the latest 100 stocks according to the TimeStamp and put them into DataFrame. Just chose values from 3 columns Time, Name, PreviousClosed (changed it to Closed). Sort documents by their ticker labels aphabetical order. Finally, append them in to `main_df`
@@ -32,20 +19,22 @@ def Processing(se):
     :param se: str
     :return: None
     """
-    data = db[f"{se}"].find({}).sort("TimeStamp", -1).limit(99)
+    global main_df
+    data = db[f"{se}"].find().sort("TimeStamp", -1).limit(100)
     df = pd.DataFrame(data)
     df = df[["Time", "Name", "PreviousClosed"]]
     df = df.rename(columns={"PreviousClosed": "Closed"})
     df = df.sort_values(by=["Name"])
-    global main_df
     main_df = main_df.append(df)
 
 
 def main():
     for se in se_lists:
         Processing(se)
-
+    main_df['Time'] = main_df['Time'] -  pd.to_timedelta(1, unit='d')
+    main_df['Time'] = pd.to_datetime(main_df['Time'].dt.strftime('%d/%m/%Y'))
 
 if __name__ == "__main__":
     main()
     main_df.to_csv("test.csv")
+    print(main_df.info())
