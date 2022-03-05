68a0ff083aadf35cfa507c5846af25dbc5e479ba
ngbnguyen <adanguyen259@gmail.com>
2022-03-05 17:11:45 +0700

    5/3/2022 Nguyen updated

--- SourceCode/CodeWeb/tradingvision/src/components/stocktable/test.js
+++ SourceCode/CodeWeb/tradingvision/src/components/stocktable/test.js
@@ -93,36 +93,7 @@ const columns = [
     },
   ];
 
-  const rows = [
-    { id: 1, ticker: 'ACB', ceiling: 305, floor: 3.7, highest: 67, lowest: 4.3, match: 0.4, volume: 2300 },
-    { id: 2, ticker: 'ASM', ceiling: 262, floor: 16.0, highest: 24, lowest: 6.0, match: 4.8, volume: 900 },
-    { id: 3, ticker: 'BVH', ceiling: 375, floor: 2.0, highest: 94, lowest: 0.0, match: 1.4, volume: 1000 },
-    { id: 4, ticker: 'DGC', ceiling: 318, floor: 0.0, highest: 81, lowest: 2.0, match: 4.8, volume: 1500 },
-    { id: 5, ticker: 'BCM', ceiling: 159, floor: 6.0, highest: 24, lowest: 4.0,match: 9.3,volume: 1200},
-    { id: 6, ticker: 'BHN', ceiling: 356, floor: 16.0, highest: 49, lowest: 3.9,match: 1.4,volume: 700},
-    { id: 7, ticker: 'VNM', ceiling: 318, floor: 0.0, highest: 81, lowest: 2.0, match: 4.8, volume: 1500 },
-    { id: 8, ticker: 'APM', ceiling: 159, floor: 6.0, highest: 24, lowest: 4.0,match: 9.3,volume: 1200},
-    { id: 9, ticker: 'HCM', ceiling: 356, floor: 16.0, highest: 49, lowest: 3.9,match: 1.4,volume: 700},
-    { id: 10, ticker: 'X20', ceiling: 318, floor: 0.0, highest: 81, lowest: 2.0, match: 4.8, volume: 1500 },
-    { id: 11, ticker: 'VRE', ceiling: 159, floor: 6.0, highest: 24, lowest: 4.0,match: 9.3,volume: 1200},
-    { id: 12, ticker: 'VIN', ceiling: 356, floor: 16.0, highest: 49, lowest: 3.9,match: 1.4,volume: 700},
   
-  ]; 
-
-// function CustomPagination() {
-//   const { state, apiRef } = useGridSlotComponentProps();
-//   const classes = useStyles();
-
-//   return (
-//     <Pagination
-//       className={classes.root}
-//       color="primary"
-//       count={state.pagination.pageCount}
-//       page={state.pagination.page + 1}
-//       onChange={(event, value) => apiRef.current.setPage(value - 1)}
-//     />
-//     );
-//   }
 // Define icons
 export function SortedDescendingIcon() {
 
@@ -169,26 +140,21 @@ const DataGridDemo = () => {
         })
     };
 
-    // const rows = stocks.map((stock) => {
-    //   return {
-    //     id: stock.Ticker,
-    //     ceiling: stock.Ceiling,
-    //     floor: stock.Floor,
-    //     highest: stock.Highest,
-    //     lowest: stock.Lowest,
-    //     match: stock.Match,
-    //     ticker: stock.Ticker,
-    //     volume: stock.Volume,
-    //   };
-    // })
-<<<<<<< HEAD
-    
-=======
-    const [pageSize, setPageSize] = React.useState(5);
+    const rows = stocks.map((stock) => {
+      return {
+        id: stock.Ticker,
+        ceiling: stock.Ceiling,
+        floor: stock.Floor,
+        highest: stock.Highest,
+        lowest: stock.Lowest,
+        match: stock.Match,
+        ticker: stock.Ticker,
+        volume: stock.Volume,
+      };
+    })
 
->>>>>>> 85952ce0a5120920722aa71ee052a5461e36fa22
-    // const rowlength = rows.length;
-    // console.log(rowlength);
+    const [pageSize, setPageSize] = React.useState(10);
+
     return (
       <ThemeProvider theme={darkTheme}>
         <div style={{ height: 600, width: '100%', color:'white'}}>
@@ -198,7 +164,7 @@ const DataGridDemo = () => {
             columns={columns}
             pageSize={pageSize}
             onPageSizeChange={(newPage) => setPageSize(newPage)}
-            rowsPerPageOptions={[5, 10, 20]}
+            rowsPerPageOptions={[10, 20, 50]}
             pagination 
             sx={{
               color: 'white',
@@ -207,7 +173,6 @@ const DataGridDemo = () => {
               fontSize: 16,
               '& .MuiTablePagination-root':{
                 color: 'white',
-               
                   },
               '& .MuiTablePagination-selectIcon': {
                 color: 'white',
