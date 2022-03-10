85a9db07a9559118bd39ccf3cf74ffecac54f033
Ngo Gia Han <han.ngo@fdssoft.com>
2022-03-10 14:26:58 +0700

    Han 10.03.2022 form validation

--- SourceCode/CodeWeb/tradingvision/src/components/reminder/SetReminderButton.js
+++ SourceCode/CodeWeb/tradingvision/src/components/reminder/SetReminderButton.js
@@ -15,25 +15,64 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DateTimePicker from '@mui/lab/DateTimePicker';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { CardContent } from '@mui/material';
+import { useForm, Form } from './useForm';
 
-
+const initialFValues = {
+  id: 0,
+  title: '',
+  content: '',
+  hireDate: new Date(),
+}
 const SetReminderButton = () => {
     const classes = useStyles()
     const [open, setOpen] = React.useState(false);
     const [date, setDate] = React.useState(new Date());
     const [value, setValue] = React.useState();
-
+    const [title, setTitle] = React.useState(false)
     const handleChange = (event) => {
       setValue(event.target.value);
     };
-
+    
     const handleClickOpen = () => {
       setOpen(true);
     };
   
     const handleClose = () => {
-      setOpen(false);
+      resetForm()
+      setOpen(false)
     };
+    const validate = (fieldValues = values) => {
+      let temp = { ...errors }
+      if ('title' in fieldValues)
+          temp.title = fieldValues.title ? "" : "This field is required."
+      if ('content' in fieldValues)
+          temp.content = fieldValues.content ? "" : "This field is required."
+          
+      setErrors({
+          ...temp
+      })
+
+      if (fieldValues == values)
+          return Object.values(temp).every(x => x == "")
+  }
+  const {
+    values,
+    setValues,
+    errors,
+    setErrors,
+    handleInputChange,
+    resetForm
+} = useForm(initialFValues, true, validate);
+      const handleSubmit = e => {
+        e.preventDefault()
+        // ham insert reminder vo database
+        if (validate()){
+            resetForm()
+            handleClose()
+        }
+        
+    }
 
     return (
       <div>
@@ -46,21 +85,31 @@ const SetReminderButton = () => {
           PaperProps={{
             style: {
               backgroundColor: 'white',
-              height: '400px',
+              height: '450px',
               width: '650px',}
           }}
         >
+        <Form onSubmit={handleSubmit}>
           <DialogTitle>
             <TextField
+            component = "form"
+              variant = "standard" //to disable outline
               required
               placeholder="Add Title" 
-              InputProps={{ disableUnderline: true,
+              noValidate
+              autoComplete="off"
+              InputProps={{ 
+                    disableUnderline: true,
                     style:{
                       fontSize:"18px",
                       fontFamily: "Montserrat",
                       color:"black",
                     }}} 
-              
+              name="title"        
+              value={values.title}
+              onChange={handleInputChange}
+              error={errors.title}
+              helperText = {errors.title}
             /> 
           </DialogTitle>
           <div className={classes.line}></div>
@@ -73,19 +122,21 @@ const SetReminderButton = () => {
               <LocalizationProvider dateAdapter={AdapterDateFns}>
                   <DateTimePicker
                     renderInput={(props) => <TextField {...props} />}
-                    label="DateTimePicker"
+                   
                     value={date}
-                    variant="inline"
+                    
                     onChange={(newValue) => {
                       setDate(newValue);
                     }}
                     className={classes.calendar}
                     inputProps={{
+                        disableUnderline: true,
                         style: {
                           color: "black",
                           fontFamily:"Montserrat",
                           fontSize:"15px",
                         },
+                        
                       }}
                   />
               </LocalizationProvider>
@@ -94,9 +145,9 @@ const SetReminderButton = () => {
             <DialogContentText>
                 <Box
                     component = "form"
-                    border={4}
-                    borderColor="black"
-                    height="170px" 
+                    // border={4}
+                    // borderColor="black"
+                    // height="170px" 
                     // sx={{
                     //   '& .MuiTextField-root': { m: 1, width: '25ch' },
                     // }}
@@ -104,15 +155,13 @@ const SetReminderButton = () => {
                     autoComplete="off"
                   >
                  <TextField
-                 
-                  error
+                  
+                  
                   required  
                   placeholder="Content"
                   multiline
-                  maxRows={5}
-                  value={value}
+                  rows={4}
                   className={classes.reminder}
-                  onChange={handleChange}
                   InputProps={{
                     disableUnderline: true,
                     style:{
@@ -120,22 +169,30 @@ const SetReminderButton = () => {
                       fontFamily: "Montserrat",
                       color:"black",
                       width:"500px",
-                    },
+                      },
                     }}
+                  variant = "outlined"
+                  size = "large"
+                  name="content"        
+                  value={values.content}
+                  onChange={handleInputChange}
+                  error={errors.content}
+                  helperText={errors.content}
                   
-                  helperText="Empty content"
                 />
               </Box>  
           </DialogContentText>
           </DialogContent>
           <DialogActions className={classes.btn}>
             <Button 
-              onClick={handleClose}
+              type = "submit"
+             
               className={classes.btn_Save}>Save</Button>
             <Button
               onClick={handleClose}
               className={classes.btn_Cancel}>Cancel</Button>
           </DialogActions>
+          </Form>
         </Dialog>
       </div>
     );
