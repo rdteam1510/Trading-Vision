6527077fd6c283bb0055e93bbc24b2a6547fd1f1
ngbnguyen <adanguyen259@gmail.com>
2022-03-17 19:38:31 +0700

    17/03/2022  Nguyen updated Line Chart

--- SourceCode/CodeWeb/backend/controllers/prediction.controller.js
+++ SourceCode/CodeWeb/backend/controllers/prediction.controller.js
@@ -7,7 +7,7 @@ exports.getPredictionByTicker = async (req, res) => {
 	const prediction = await Prediction.find({
 		Ticker: req.params.ticker,
 	})
-		.sort({ TimeStamp: -1, Date: -1 })
+		.sort({ TimeStamp: -1, Date: 1 })
 		.limit(5);
 	if (!prediction) {
 		throw new NotFoundError("Cannot find prediction with this ticker");
