a1707595cbc6c33d8ac23907f124436700b61137
ngbnguyen <adanguyen259@gmail.com>
2022-03-17 11:28:36 +0700

    17/03/2022 Nguyen updated Profile page

--- SourceCode/CodeWeb/tradingvision/src/pages/profile/Profile.js
+++ SourceCode/CodeWeb/tradingvision/src/pages/profile/Profile.js
@@ -1,38 +1,74 @@
 import { Typography,
         Avatar,
         Grid,
+        Button,
 } from '@material-ui/core'
-import React from 'react'
+import React, { useState } from 'react'
 import useStyles from './style'
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+
 
 const Profile = ({user}) => {
   const classes = useStyles()
-
-
+  
   return (
     <div className={classes.container}>
-    <Typography variant="h5" className={classes.title}>
-      User Profile
-    </Typography>
-    <Grid container spacing={2}  className={classes.Grid}>
-      <Grid item>
-        <Avatar 
-        src={user.image} 
-        alt="image"
-        className={classes.avatar}
-        />
-      </Grid>
-      <Grid item className={classes.info_grid}>
-        <Typography gutterBottom component="div" className={classes.info} >
-                  {user.displayName}
-        </Typography>
-        <Typography gutterBottom  className={classes.info}>
-          {user.email}
-        </Typography>
 
-      </Grid>
+    {user ? (
+      <>
+        <Typography variant="h5" className={classes.title}>
+          User Profile
+        </Typography>
+        <Grid container spacing={2}  className={classes.Grid}>
+          <Grid item alignItems="center">
+          <Avatar 
+            src={user.image} 
+            alt="image"
+            className={classes.avatar}
+            />
+          </Grid>
+          <Grid item className={classes.info_grid}>
+            <Typography gutterBottom className={classes.username} >
+                      {user.displayName}
+            </Typography>
+            <Typography gutterBottom  className={classes.info}>
+              {user.email}
+            </Typography>
 
-    </Grid>
+          </Grid>
+
+        </Grid>
+        <div className={classes.delete_account}>
+        <Grid container spacing={2}  className={classes.Grid_delete}>
+          <Grid item align="center">
+            <HighlightOffIcon
+            sx={{fontSize: 90}}
+            />
+            <Typography className={classes.delete_title}>
+              Delete Account
+            </Typography>
+          </Grid>
+          <Grid item className={classes.delete_warning}>
+            <Typography className={classes.delete_permanent} >
+                      Permanently delete 
+            </Typography>
+            <Typography className={classes.warning}>
+                      Be careful - All of your data and information will be deleted.
+            </Typography>
+            <Button className={classes.btn}> Delete All</Button>
+
+          </Grid>
+
+        </Grid>
+
+        </div>
+
+    </>
+
+    ):(
+      <></>
+    )}
+    
     
 
 
--- SourceCode/CodeWeb/tradingvision/src/pages/profile/style.js
+++ SourceCode/CodeWeb/tradingvision/src/pages/profile/style.js
@@ -1,6 +1,6 @@
 import {makeStyles} from '@material-ui/core'
 
-const useStyles = makeStyles(()=>({
+const useStyles = makeStyles((theme) =>({
     container: {
         marginLeft:"9%",
         marginBottom: "15%",
@@ -15,18 +15,70 @@ const useStyles = makeStyles(()=>({
         marginBottom:"5%"
     },
     avatar:{
-        height: "150px",
-        width: "150px",
-        marginTop: "3%",
+        height: "120px",
+        width: "120px",
+        [theme.breakpoints.down('sm')]:{
+            height: "80px",
+            width: "80px",
+        },
+        
     },
     info_grid:{
         marginLeft:"5%",
+        marginTop: "1%",
+        
     },
     info:{
         fontSize: "18px", 
-        marginTop: "10px",
+        
         fontFamily: "Montserrat",
+        padding: "2%",
     },
+    username:{
+        marginTop: "10px",
+        fontWeight: 'bold', 
+        fontFamily: "Montserrat",
+        marginBottom: "15%",
+        fontSize: "22px",
+    },
+    Grid_delete:{
+        marginTop: "10%"
+    },
+    delete_title:{
+        fontFamily: "Montserrat",
+        fontSize: "13px"
+    },
+    delete_warning:{
+        backgroundColor: "#373A6D",
+        marginLeft:"3%",
+        marginTop:"-1%",
+        width: "80%",
+        height: "150px",
+        borderRadius: "5px",
+    },
+    delete_permanent:{
+        fontWeight: 'bold', 
+        fontFamily: "Montserrat",
+        fontSize: "16px",
+        marginLeft:"2%", 
+        marginTop:"1%"
+    },
+    warning:{
+        fontFamily: "Montserrat",
+        fontSize: "14px",
+        marginLeft:"2%", 
+        marginTop:"15px"
+    },
+    btn:{
+        backgroundColor: "#F12323",
+        borderRadius: "7px",
+        color: "#FFFFFF",
+        marginLeft:"2%", 
+        marginTop: "14px",
+        fontFamily: "Montserrat",
+        fontWeight: "600",
+    }
+
 
 }))
 
