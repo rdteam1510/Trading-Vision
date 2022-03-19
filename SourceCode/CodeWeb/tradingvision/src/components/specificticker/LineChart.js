0b313a0b776f53b3ecc46061d377efea952fa7db
ngbnguyen <adanguyen259@gmail.com>
2022-03-19 15:16:10 +0700

    19/3/2022 Nguyen updated compared line chart

--- SourceCode/CodeWeb/tradingvision/src/components/specificticker/LineChart.js
+++ SourceCode/CodeWeb/tradingvision/src/components/specificticker/LineChart.js
@@ -1,119 +1,417 @@
-import React, {useState, useEffect} from 'react';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import ReactHighcharts from 'react-highcharts/ReactHighstock.src'
+import moment from 'moment'
 import { useParams } from 'react-router-dom';
+import axios from 'axios'
 
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  );
 
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
+const LineChart =() =>{
+    const compareTicker = 'VIC'
+    const {ticker} = useParams()
+    const [data, setData] = useState([])
+    const [predictions, setPredictions] = useState([])
+    const [compare, setCompare] = useState([])
+    const [comparePredictions, setComparePredictions] = useState([])
+    const priceData =[]
+    const predictPrice=[]
+    const compareData =[]
+    const predictCompare=[]
+
+
+    useEffect(() => {
+      actualPrice()
+      currentStockPredictions()
+      compareStocks()
+      compareStockPredictions()
+    },[])
+  
+    const actualPrice = async() => {
+      axios.get(`/api/forpredictions/${ticker}`)
+      .then((response) =>{
+        setData(response.data)
+      })
+    }
+  
+    const currentStockPredictions = async() => {
+      axios.get(`/api/predictions/${ticker}`)
+      .then((response) =>{
+        setPredictions(response.data.prediction)
+      })
+    }
+
+    const compareStocks = async()=>{
+      axios.get(`/api/forpredictions/${compareTicker}`)
+      .then((response) =>{
+        setCompare(response.data)
+      })
+    }
+
+    const compareStockPredictions = async() => {
+      axios.get(`/api/predictions/${compareTicker}`)
+      .then((response) =>{
+        setComparePredictions(response.data.prediction)
+      })
+    }
+
+    // actual price list
+    data.map(point => {
+        priceData.push([
+          point.Time*1000,
+          point.Close,])
+        
+    })
+    
+    // current stock's prediction price list 
+    predictions.map(point => {
+      predictPrice.push([
+        Date.parse(point.Date),
+        point.PredictedPrice
+      ])
+    })
+    priceData.push(predictPrice[0])
+
+    if (compareTicker !== ''){
+        // compared ticker's price list
+      compare.map(point=>{
+        compareData.push([
+          point.Time*1000,
+          point.Close,])
+      })
+
+      // compared stock's prediction price list
+      comparePredictions.map(point => {
+        predictCompare.push([
+          Date.parse(point.Date),
+          point.PredictedPrice
+        ])
+      })
+      compareData.push(predictCompare[0])
+    }
+   
+
+    const options = {currency: 'VND'};
+    const numberFormat = new Intl.NumberFormat('en-US', options);
+    const configPrice = {
+      yAxis: [{
+        offset: 20,
+
+        labels: {
+          formatter: function () {
+            return numberFormat.format(this.value) 
+          }
+          ,
+          x:-15,
+          style: {
+            "color": "#fff",
+            "position": "absolute",
+          },
+          align: 'left',
+        },
+        
+      },
+        
+      ],
+      tooltip: {
+        shared: false,
+        // formatter: function () {
+        //   return numberFormat.format(this.y, 0) +  '</b><br/>' + moment(this.x).format('MMMM Do YYYY')
+        // },
+        split: true,
+        
+      },
+      plotOptions: {
+        series: {
+          showInNavigator: true,
+          gapSize: 6,
+        
+        }
+      },
+      rangeSelector: {
+        selected: 1
+        
       },
       title: {
-        display: true,
-        text: 'Chart.js Line Chart',
+        text: `${ticker} stock price`,
+        style: {
+          color: '#F2F1F0',
+          fontSize: '18px'
+        }
       },
- 
-
-    },
-  };
-
-   const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September'];
+      chart: {
+        height: 600,
+      },
   
+      credits: {
+        enabled: false
+      },
   
-
+      legend: {
+        enabled: true,
+        itemStyle:{
+            color: '#F2F1F0',
+            fontSize: '13px'
+        },
+        itemHoverStyle:{
+          color: '#E5E8E8',
+        },
+      },
+      xAxis: {
+        type: 'date',
+        labels: {
+          style: {
+            "color": "#F2F1F0",
+            "position": "absolute",
+          },
+        }
+      },
+      rangeSelector: {
+        buttons: [{
+          type: 'day',
+          count: 1,
+          text: '1d',
+        }, {
+          type: 'day',
+          count: 7,
+          text: '7d'
+        }, {
+          type: 'month',
+          count: 1,
+          text: '1m'
+        }, {
+          type: 'month',
+          count: 3,
+          text: '3m'
+        },
+          {
+          type: 'all',
+          text: 'All'
+        }],
+        selected: 4, 
+        buttonTheme: {
+          fill: "rgba(0,0,0,1)",
+          'stroke-width': 0,
+          style: {
+            color: 'grey',
+        },
+        states: {
+            hover: {
+            },
+            select: {
+                fill: '#FD7F20',
+                style: {
+                    color: '#fff',
+                    fontWeight: 'bold'
+                }
+            }
+        }},
+        inputStyle: {
+          color: 'grey',
+          fontWeight: 'bold'
+      },
+      
+      },
+      series: [{
+        name: `${ticker} `,
+        type: 'spline',
   
-
-const LineChart = () => {
-  const {ticker} = useParams()
-  const [data, setData] = useState([])
-  const [predictions, setPredictions] = useState([])
-  useEffect(() => {
-    componentDidMount()
-    componentDidMountPredictions()
-  },[])
-
-  const componentDidMount = async() => {
-    axios.get(`/api/stocks/${ticker}`)
-    .then((response) =>{
-      setData(response.data.stock)
-    })
-  }
-
-  const componentDidMountPredictions = async() => {
-    axios.get(`/api/predictions/${ticker}`)
-    .then((response) =>{
-      setPredictions(response.data.prediction)
-    })
-  }
-  console.log(predictions);
-
-
-  const labels = data.map(dt => 
-    // dt.TimeStamp
-    (new Date(dt.TimeStamp*1000)).toLocaleDateString('en-US')
-    )
-  // const date = ['2022-07-01', '2022-07-02', '2022-07-04', '2022-07-05', '2022-07-06', '2022-07-07', '2022-07-08', '2022-07-09', '2022-07-10', '2022-07-11', '2022-07-12', '2022-07-13', '2022-07-14']
-
-  const price = data.map(dt =>dt.Lowest)
-
-  const info = {
-    labels,
-    datasets: [
+        data: priceData,
+        tooltip: {
+          valueDecimals: 2
+        },
+        color: '#04F9F2',
+  
+      },
       {
-        label: 'ticker - one',
-        data:  price,
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        name: `${ticker} Predicted Price`,
+        type: 'spline',
+  
+        data: predictPrice,
+        tooltip: {
+          valueDecimals: 2
+        },
+        color: '#CFE9EF',
+  
+      },
+      {
+        name: `${compareTicker}`,
+        type: 'spline',
+  
+        data: compareData,
+        tooltip: {
+          valueDecimals: 2
+        },
+        color: '#F51663',
+  
+      },
+      {
+        name: `${compareTicker} Predicted Price`,
+        type: 'spline',
+  
+        data: predictCompare,
+        tooltip: {
+          valueDecimals: 2
+        },
+        color: '#EAC5B0',
+  
+      }
+      ],
+      
+    };
+
+    const configPriceWithoutCompare = {
+      
+      yAxis: [{
+        offset: 20,
+
+        labels: {
+          formatter: function () {
+            return numberFormat.format(this.value) 
+          }
+          ,
+          x:-15,
+          style: {
+            "color": "#fff",
+            "position": "absolute",
+          },
+          align: 'left',
+        },
         
       },
-      // {
-      //   label: 'predict one',
-      //   data:  [null, null,null, null, 15162, 95072, 95072, 181455, 4353],
-      //   borderColor: 'cyan',
-      //   backgroundColor: 'rgba(255, 99, 132, 0.5)',
         
-      // },
-      // {
-      //   label: 'ticker - two',
-      //   data:  [87902, 300000, 693060, 200000, 590000, null,null, null, null],
-      //   borderColor: 'rgb(53, 162, 235)',
-      //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      // },
-      // {
-      //   label: 'predict - two',
-      //   data:  [null, null,null, null, 590000, 25000, 65792, 76890, 109022],
-      //   borderColor: 'yellow',
-      //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      // },
-    ],
-  };
+      ],
+      tooltip: {
+        shared: false,
+        // formatter: function () {
+        //   return numberFormat.format(this.y, 0) +  '</b><br/>' + moment(this.x).format('MMMM Do YYYY')
+        // },
+        split: true,
+        
+      },
+      plotOptions: {
+        series: {
+          showInNavigator: true,
+          gapSize: 6,
+        
+        }
+      },
+      rangeSelector: {
+        selected: 1
+        
+      },
+      title: {
+        text: `${ticker} stock price`,
+        style: {
+          color: '#F2F1F0',
+          fontSize: '18px'
+        }
+      },
+      chart: {
+        height: 600,
+      },
+  
+      credits: {
+        enabled: false
+      },
+  
+      legend: {
+        enabled: true,
+        itemStyle:{
+            color: '#F2F1F0',
+            fontSize: '13px'
+        },
+        itemHoverStyle:{
+          color: '#E5E8E8',
+        },
+      },
+      xAxis: {
+        type: 'date',
+        labels: {
+          style: {
+            "color": "#F2F1F0",
+            "position": "absolute",
+          },
+        }
+      },
+      rangeSelector: {
+        buttons: [{
+          type: 'day',
+          count: 1,
+          text: '1d',
+        }, {
+          type: 'day',
+          count: 7,
+          text: '7d'
+        }, {
+          type: 'month',
+          count: 1,
+          text: '1m'
+        }, {
+          type: 'month',
+          count: 3,
+          text: '3m'
+        },
+          {
+          type: 'all',
+          text: 'All'
+        }],
+        selected: 4, 
+        buttonTheme: {
+          fill: "rgba(0,0,0,1)",
+          'stroke-width': 0,
+          style: {
+            color: 'grey',
+        },
+        states: {
+            hover: {
+            },
+            select: {
+                fill: '#FD7F20',
+                style: {
+                    color: '#fff',
+                    fontWeight: 'bold'
+                }
+            }
+        }},
+        inputStyle: {
+          color: 'grey',
+          fontWeight: 'bold'
+      },
+      
+      },
+      series: [{
+        name: `${ticker} `,
+        type: 'spline',
+  
+        data: priceData,
+        tooltip: {
+          valueDecimals: 2
+        },
+        color: '#04F9F2',
+  
+      },
+      {
+        name: `${ticker} Predicted Price`,
+        type: 'spline',
+  
+        data: predictPrice,
+        tooltip: {
+          valueDecimals: 2
+        },
+        color: '#CFE9EF',
+  
+      },]
+    };
+    return (
+      <div>
+      {compareTicker === '' ? (
+        <ReactHighcharts config = {configPriceWithoutCompare}></ReactHighcharts>
+      ):(
+        <ReactHighcharts config = {configPrice}></ReactHighcharts>
+      )}
+        
+      </div>
+    )
+  }
 
-  return (
-    <div>
-       <Line options={options} data={info} />
-    </div>
-  )
-}
 
-export default LineChart;
+export default LineChart
