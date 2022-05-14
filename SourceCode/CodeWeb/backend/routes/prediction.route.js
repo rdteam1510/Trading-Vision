f2754ff75be01f065297ea6358b112c82ec8fa3f
ngbnguyen <adanguyen259@gmail.com>
2022-05-14 12:32:17 +0700

    14/5/2022  Nguyen updated

--- SourceCode/CodeWeb/backend/routes/prediction.route.js
+++ SourceCode/CodeWeb/backend/routes/prediction.route.js
@@ -1,13 +1,19 @@
 // Import modules
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const {
 	getPredictionByTicker,
-} = require("../controllers/prediction.controller");
+	getAllPredictionByTicker,
+} = require('../controllers/prediction.controller');
+
+// @desc Get 5 predictions
+// @method GET
+// @route /api/predictions
+router.route('/:ticker').get(getPredictionByTicker);
 
 // @desc Get all predictions
 // @method GET
-// @route /api/predictions
-router.route("/:ticker").get(getPredictionByTicker);
+// @route /api/predictions/all
+router.route('/all/:ticker').get(getAllPredictionByTicker);
 
 module.exports = router;
