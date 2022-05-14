06b54d5354ba1e7c72284aa29d61177e3fd42609
ngbnguyen <adanguyen259@gmail.com>
2022-05-14 11:07:29 +0700

    Revert "13/5/22 Minh updates Predictions"
    This reverts commit 9f5ec9c20381e1b76225c38bf1a821c362195540.

--- SourceCode/CodeWeb/backend/controllers/prediction.controller.js
+++ SourceCode/CodeWeb/backend/controllers/prediction.controller.js
@@ -1,35 +1,16 @@
 // Import modules
-const { NotFoundError } = require('../errors');
-const Prediction = require('../models/Prediction');
+const { NotFoundError } = require("../errors");
+const Prediction = require("../models/Prediction");
 
-// Get 5 predictions by Ticker
+// Get predictions
 exports.getPredictionByTicker = async (req, res) => {
-	try {
-		const prediction = await Prediction.find({
-			Ticker: req.params.ticker,
-		})
-			.sort({ TimeStamp: -1, Date: 1 })
-			.limit(5);
-		if (!prediction) {
-			throw new NotFoundError('Cannot find prediction with this ticker');
-		}
-		return res.json({ success: true, prediction });
-	} catch (error) {
-		return res.status(500).json({ error: error.message });
+	const prediction = await Prediction.find({
+		Ticker: req.params.ticker,
+	})
+		.sort({TimeStamp: -1, Date: 1})
+		.limit(5);
+	if (!prediction) {
+		throw new NotFoundError("Cannot find prediction with this ticker");
 	}
-};
-
-// Get all predictions by Ticker
-exports.getAllPredictionByTicker = async (req, res) => {
-	try {
-		const prediction = await Prediction.find({
-			Ticker: req.params.ticker,
-		}).sort({ TimeStamp: -1, Date: 1 });
-		if (!prediction) {
-			throw new NotFoundError('Cannot find prediction with this ticker');
-		}
-		return res.json({ success: true, prediction });
-	} catch (error) {
-		return res.status(500).json({ error: error.message });
-	}
+	return res.json({ success: true, prediction });
 };
--- SourceCode/CodeWeb/backend/routes/deleteUser.route.js
+++ SourceCode/CodeWeb/backend/routes/deleteUser.route.js
@@ -1,10 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { deleteUser } = require('../controllers/deleteUser.controller');
+const { deleteUser } = require("../controllers/deleteUser.controller");
 
-// @desc delete all user info
-// method: GET
-// route: /api/deleteuser
-router.route('/').get(deleteUser);
+router.route("/").get(deleteUser);
 
 module.exports = router;
