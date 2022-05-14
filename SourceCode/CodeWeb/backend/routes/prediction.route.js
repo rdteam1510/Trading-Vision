4d0fc91029aca9879be2d0fb2406fe4981f182ae
ngbnguyen <adanguyen259@gmail.com>
2022-05-14 12:19:46 +0700

    Revert "13/5/22 Minh updates Predictions"
    This reverts commit 9f5ec9c20381e1b76225c38bf1a821c362195540.

--- SourceCode/CodeWeb/backend/routes/prediction.route.js
+++ SourceCode/CodeWeb/backend/routes/prediction.route.js
@@ -1,19 +1,13 @@
 // Import modules
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
 	getPredictionByTicker,
-	getAllPredictionByTicker,
-} = require('../controllers/prediction.controller');
-
-// @desc Get 5 predictions
-// @method GET
-// @route /api/predictions
-router.route('/:ticker').get(getPredictionByTicker);
+} = require("../controllers/prediction.controller");
 
 // @desc Get all predictions
 // @method GET
-// @route /api/predictions/all
-router.route('/all/:ticker').get(getAllPredictionByTicker);
+// @route /api/predictions
+router.route("/:ticker").get(getPredictionByTicker);
 
 module.exports = router;
